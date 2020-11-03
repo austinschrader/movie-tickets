363ef1430f6955cf67c35c5445620203f5acd677
Austin Schrader <austin.jschrader@gmail.com>
2020-11-03 15:26:53 -0800

    Add the movies to the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,42 @@ function Title(movieName, releaseDate, requiredAge, movieTimes) {
 	this.movieTimes = movieTimes;
 }
 
+let movieTimes = [];
+
+Title.prototype.movieInfo = function () {
+	return (
+		this.movieName +
+		'; ' +
+		'Release date: ' +
+		this.releaseDate +
+		'; ' +
+		'Age Requirement: ' +
+		this.requiredAge +
+		'; ' +
+		'Show Times: ' +
+		this.movieTimes
+	);
+};
+
+let avengersTitle = new Title('Avengers', 2019, 18, [
+	'1:00PM',
+	'4:00PM',
+	'8:00PM',
+]);
+let titanicTitle = new Title('Titanic', 1997, 13, [
+	'11:00AM',
+	'3:30PM',
+	'8:00PM',
+]);
+let cocoTitle = new Title('Coco', 2017, 0, [
+	'10:30AM',
+	'1:00PM',
+	'4:00PM',
+	'7:00PM',
+]);
+
+//Movie Title, 20xx, Age Requirement: x+, Show Times:
+
 //ticketPurchase.prototype.function();
 
 // If/Else Statement
@@ -30,26 +66,25 @@ function Title(movieName, releaseDate, requiredAge, movieTimes) {
 // time is during matinee then the price is 50% off
 // if the users age is 60+ then the price is 50% off
 
-// User Interface Logic
-// Instantiate the MoviesList object
-let moviesList = new MoviesList();
-// Instantiate the Title() object
-let avengersTitle = new Title('Avengers', 2019, 18, '1:00PM');
-// Console Log that Avengers information
-console.log(avengersTitle.movieName);
-console.log(avengersTitle.releaseDate);
-console.log(avengersTitle.requiredAge);
-console.log(avengersTitle.movieTimes);
+// // User Interface Logic
+// // Instantiate the MoviesList object
+// let moviesList = new MoviesList();
+// // Instantiate the Title() object
+
+// // Console Log that Avengers information
+
+// console.log(avengersTitle.movieName);
+// console.log(avengersTitle.releaseDate);
+// console.log(avengersTitle.requiredAge);
+// console.log(avengersTitle.movieTimes);
 
 $(document).ready(function () {
-	$('.movie-options').append(avengersTitle.movieName);
-	$('.movie-options').append(', released in ' + avengersTitle.releaseDate);
-	$('.movie-options').append(
-		' with a required age of  ' + avengersTitle.requiredAge + '.'
-	);
-	$('.movie-options').append(
-		' The available time(s) are: ' + avengersTitle.movieTimes + ' '
-	);
+	$('#click-me').click(function () {
+		$('#show-movies').empty().toggle();
+		$('#show-movies').append(avengersTitle.movieInfo() + '</br></br>');
+		$('#show-movies').append(titanicTitle.movieInfo() + '</br></br>');
+		$('#show-movies').append(cocoTitle.movieInfo() + '</br></br>');
+	});
 
 	$('#formOne').submit(function () {
 		event.preventDefault();
