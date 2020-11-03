35db15d8cce48d9085d94804f99afb0ce812eaa8
Austin Schrader <austin.jschrader@gmail.com>
2020-11-03 13:35:16 -0800

    Add the functionality for the avengersTitle instantiation of the title object
    Co-authored-by: Danielle Thompson <71739178+dani-t-codes@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,8 @@ function MoviesList() {
 	this.currentId = 0;
 }
 
-// Business Logic for Titles
-function Titles() {
+// Business Logic for a Title
+function Title(movieName, releaseDate, requiredAge, movieTimes) {
 	this.movieName = movieName;
 	this.releaseDate = releaseDate;
 	this.requiredAge = requiredAge;
@@ -31,9 +31,26 @@ function Titles() {
 // if the users age is 60+ then the price is 50% off
 
 // User Interface Logic
+// Instantiate the MoviesList object
 let moviesList = new MoviesList();
+// Instantiate the Title() object
+let avengersTitle = new Title('Avengers', 2019, 18, '1:00PM');
+// Console Log that Avengers information
+console.log(avengersTitle.movieName);
+console.log(avengersTitle.releaseDate);
+console.log(avengersTitle.requiredAge);
+console.log(avengersTitle.movieTimes);
 
 $(document).ready(function () {
+	$('.movie-options').append(avengersTitle.movieName);
+	$('.movie-options').append(', released in ' + avengersTitle.releaseDate);
+	$('.movie-options').append(
+		' with a required age of  ' + avengersTitle.requiredAge + '.'
+	);
+	$('.movie-options').append(
+		' The available time(s) are: ' + avengersTitle.movieTimes + ' '
+	);
+
 	$('#formOne').submit(function () {
 		event.preventDefault();
 		let userInputTitle = $('#userInputTitle').val();
