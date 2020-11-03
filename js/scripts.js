b5bb47b4210851958d82b2e51f6aae441eaa21ed
Austin Schrader <austin.jschrader@gmail.com>
2020-11-03 11:56:57 -0800

    Added in variables to extract the values from the form and append them to the DOM
    Co-authored-by: Danielle Thompson <71739178+dani-t-codes@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,14 @@
 $(document).ready(function () {
 	$('#formOne').submit(function () {
 		event.preventDefault();
-		let userInput = $('#userInput').val();
-		$('.userResults').append(userInput);
+		let userInputTitle = $('#userInputTitle').val();
+		let userInputTime = $('#userInputTime').val();
+		let userInputAge = $('#userInputAge').val();
+
+		$('.userResults').append(userInputTitle);
+		$('.userResults').append(userInputTime);
+		$('.userResults').append(userInputAge);
+
 		// numberTo(userInput);
 	});
 });
